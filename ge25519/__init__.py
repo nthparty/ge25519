365ce139e97137246831839739730a21b948e4c5
Andrei Lapets <a@lapets.io>
2020-03-22 21:20:39 -0400

    Allow users access to all subclasses.

--- ge25519/__init__.py
+++ ge25519/__init__.py
@@ -1,2 +1,5 @@
 # Allow users to build ge25519 values directly.
-from ge25519.ge25519 import ge25519
+from ge25519.ge25519 import\
+    ge25519,\
+    ge25519_p2, ge25519_p3, ge25519_p1p1,\
+    ge25519_precomp, ge25519_cached
