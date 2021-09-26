22026d00b7411036e3823ae02920cf02c5bad563
Andrei Lapets <a@lapets.io>
2021-09-25 23:19:04 -0400

    Add links, expand descriptions, and fix typos in README text; update license.

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
     url="https://github.com/nthparty/ge25519",
     author="Andrei Lapets",
     author_email="a@lapets.io",
-    description="Native Python implementation of Ed25519 "+\
-                "group elements and operations.",
+    description="Pure Python data structure for working with Ed25519 "+\
+                "(and Ristretto) group elements and operations.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     test_suite="nose.collector",
