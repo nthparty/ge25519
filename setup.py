53a59f0e2d06e7b755149f52204c3e868b8ebc4e
Andrei Lapets <a@lapets.io>
2021-09-25 23:38:23 -0400

    Add config, template, and build files for auto-generating docs; update README.

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
 with open("README.rst", "r") as fh:
-    long_description = fh.read()
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+# The lines below are parsed by `docs/conf.py`.
+name = "ge25519"
+version = "0.2.0"
 
 setup(
-    name="ge25519",
-    version="0.2.0",
-    packages=["ge25519",],
+    name=name,
+    version=version,
+    packages=[name,],
     install_requires=[
         "fe25519>=0.2.0",
         "parts>=0.2.1",
