cfffc530af396ff7c218199021d92d9d0b25341f
Andrei Lapets <a@lapets.io>
2023-08-26 15:18:15 -0400

    Adjust docstring in module directory import file.

--- src/ge25519/__init__.py
+++ src/ge25519/__init__.py
@@ -1,5 +1,5 @@
-"""Gives users direct access to classes."""
-from ge25519.ge25519 import\
-    ge25519,\
-    ge25519_p2, ge25519_p3, ge25519_p1p1,\
+"""Allow users to access the classes directly."""
+from ge25519.ge25519 import \
+    ge25519, \
+    ge25519_p2, ge25519_p3, ge25519_p1p1, \
     ge25519_precomp, ge25519_cached
